fec7d0bb2ecca154af48e3fdb21a1561bb19baf3
alihaider123go <alihaider123go@gmail.com>
2024-07-10 11:47:21 +0500

    deploy contract in test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,23 @@
 const assert = require('assert');
 const ganache = require('ganache');
-const { beforeEach } = require('mocha');
 const { Web3 } = require('web3');
-
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async()=>{
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    // use one of those accounts to deploy the smart contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:bytecode, arguments:['Hi there!']})
+    .send({from:accounts[0],gas:1000000})
+})
 
 
-// class Car{
-//     park(){
-//         return "stopped";
-//     }
-//     drive(){
-//         return "vroom";
-//     }
-// }
-
-// let car;
-// beforeEach(()=>{
-//     car = new Car();
-// });
-
-// describe('Car',()=>{
-//     it('can park',()=>{        
-//         assert.equal(car.park(),'stopped');
-//     });
-//     it('can drive',()=>{
-//         assert.equal(car.drive(),'vroom');
-//     });
-    
-// });
+describe('Inbox',()=>{
+    it('deploys a contract', ()=>{
+        console.log(inbox);
+    }); 
+});
